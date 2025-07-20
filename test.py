e7af06390666c4545733b9ceb56a40f678ea4464
status.call.h8r <222740670+statuscallh8r@users.noreply.github.com>
2025-07-20 23:04:56 +0300

    Refactor the test runner. Support filtering tests by glob. Add flags for only compiling and only executing, turning the test runner into a simple API for manual testing. Allow passing manual input via stdin. Improve test names to also include the definition directory path.

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
+import fnmatch
 import json
+import sys
 import threading
 import time
 import unittest
@@ -62,25 +64,39 @@ def run_with_input(exec_cmd: List[str], stdin: str, line_delayed: bool = True) -
 
     return proc.returncode, "".join(output_lines), "".join(error_lines)
 
-@dataclass
+@dataclass(kw_only=True)
 class TestCase:
-    case: Path
-    code: Path
+    name: str = ""
+    def_path: Path
+    case_path: Path
+    code_path: Path
 
 import json
 from pathlib import Path
 from typing import TypeAlias
 from itertools import product
 
-def discover_tests() -> list[TestCase]:
+def discover_tests(args) -> list[TestCase]:
     def resolve(pattern: str, base: Path) -> list[Path]:
         if not pattern:
             raise ValueError(f"Empty pattern in {base}")
         return list(base.glob(pattern)) if "*" in pattern else [base / pattern]
 
+    import re
+
+    glob = lambda x: True
+    if args.glob:
+        pattern = fnmatch.translate(f"**{args.glob}**") # TODO - might fail with --glob anagra*
+        glob = re.compile(pattern)
+        glob = glob.match
+
     tests: list[TestCase] = []
 
     for tests_json_path in TEST_ROOT.rglob("tests.json"):
+        if not glob(str(tests_json_path)):
+            print(f"ignoring `{tests_json_path}` per midglob `{args.glob}`")
+            continue
+
         with open(tests_json_path, "r") as f:
             # TODO - instead of JSON leverage our own format
             entries = json.load(f)
@@ -95,76 +111,102 @@ def discover_tests() -> list[TestCase]:
             case_paths = resolve(case_glob, base_dir)
 
             for code_path, case_path in product(code_paths, case_paths):
-                tests.append(TestCase(case=case_path, code=code_path))
+                tests.append(TestCase(def_path=base_dir, case_path=case_path, code_path=code_path))
 
     return tests
 
-def make_test_method(tc: TestCase):
+def make_test_method(tc: TestCase, args):
     def test(self: unittest.TestCase) -> None:
-        case_dir = tc.case
-        code_dir = tc.code
+        case_dir = tc.case_path
+        code_dir = tc.code_path
+        print(tc.name)
         print(f"running test for {tc}")
         expected_compile_err = read_file(case_dir / "compile.stderr")
         expected_runtime_err = read_file(case_dir / "runtime.stderr")
         expected_stdout = read_file(case_dir / "success.stdout")
-        stdin_text = read_file(case_dir / "stdin")
-        print("stdin=")
-        print(stdin_text)
+        if args.stdin:
+            stdin_text = sys.stdin.read()
+        else:
+            stdin_text = read_file(case_dir / "stdin")
 
         with tempfile.TemporaryDirectory() as tmpdir_str:
             tmpdir = Path(tmpdir_str)
             out_path = code_dir / EXECUTABLE
+
+            # TODO - is this obsolete? what are we gaining from this copy ?
             shutil.copytree(code_dir, tmpdir, dirs_exist_ok=True)
             shutil.copytree(case_dir, tmpdir, dirs_exist_ok=True)
 
-            compiler_path = Path("compiler/src/main.py")
-            print(f"{case_dir}: compiling...")
-            compile_cmd = [compiler_path.absolute(), tmpdir.absolute(), out_path.absolute()]
-            compile_proc = subprocess.run(compile_cmd, cwd=tmpdir, capture_output=True, text=True)
-            print(f"{case_dir}: done compiling.")
+            if args.compile:
+                compiler_path = Path("compiler/src/main.py")
+                print(f"{case_dir}: compiling...")
+                compile_cmd = [compiler_path.absolute(), tmpdir.absolute(), out_path.absolute()]
+                compile_proc = subprocess.run(compile_cmd, cwd=tmpdir, capture_output=True, text=True)
+                print(f"{case_dir}: done compiling.")
 
-            if expected_compile_err is not None:
-                self.assertEqual(
-                    compile_proc.stderr.strip(),
-                    expected_compile_err.strip(),
-                    msg="Compile stderr mismatch"
-                )
-                self.assertNotEqual(compile_proc.returncode, 0, msg="Expected compile to fail")
-                return
+                if expected_compile_err is not None:
+                    self.assertEqual(
+                        compile_proc.stderr.strip(),
+                        expected_compile_err.strip(),
+                        msg="Compile stderr mismatch"
+                    )
+                    self.assertNotEqual(compile_proc.returncode, 0, msg="Expected compile to fail")
+                    return
 
-            self.assertEqual(compile_proc.returncode, 0, msg=f"Compile failed unexpectedly\n{compile_proc.stderr}")
+                self.assertEqual(compile_proc.returncode, 0, msg=f"Compile failed unexpectedly\n{compile_proc.stderr}")
 
-            exec_cmd = ["node", out_path.absolute()]
-            print(f"{case_dir}: running...")
-            returncode, stdout, stderr = run_with_input(exec_cmd, stdin=stdin_text, line_delayed=False)
-            print(f"{case_dir}: complete.")
-            print(stdout)
+            if args.run:
+                exec_cmd = ["node", out_path.absolute()]
+                print(f"{case_dir}: running...")
+                returncode, stdout, stderr = run_with_input(exec_cmd, stdin=stdin_text, line_delayed=False)
+                print(f"{case_dir}: complete.")
+                print(stdout)
 
-            if expected_runtime_err is not None:
-                self.assertEqual(
-                    stderr.strip(),
-                    expected_runtime_err.strip(),
-                    msg="Runtime stderr mismatch"
-                )
-                self.assertNotEqual(returncode, 0, msg="Expected runtime failure")
-            else:
-                self.assertEqual(
-                    stdout.strip(),
-                    expected_stdout.strip() if expected_stdout else "",
-                    msg="Runtime stdout mismatch"
-                )
-                self.assertEqual(returncode, 0, msg="Runtime failed unexpectedly")
+                if args.stdin:
+                    pass # custom input? don't assert.
+                else:
+                    if expected_runtime_err is not None:
+                        self.assertEqual(
+                            stderr.strip(),
+                            expected_runtime_err.strip(),
+                            msg="Runtime stderr mismatch"
+                        )
+                        self.assertNotEqual(returncode, 0, msg="Expected runtime failure")
+                    else:
+                        self.assertEqual(
+                            stdout.strip(),
+                            expected_stdout.strip() if expected_stdout else "",
+                            msg="Runtime stdout mismatch"
+                        )
+                        self.assertEqual(returncode, 0, msg="Runtime failed unexpectedly")
     return test
 
-# Dynamically create test methods
-class MyLangTestCase(unittest.TestCase): pass
-
-for tc in discover_tests():
-    code_parts = tc.code.relative_to(TEST_ROOT).parts
-    case_parts = tc.case.relative_to(TEST_ROOT).parts
-    name = "test_" + "_".join(code_parts + case_parts)
-    setattr(MyLangTestCase, name, make_test_method(tc))
-
 if __name__ == "__main__":
+    # Dynamically create test methods
+    class MyLangTestCase(unittest.TestCase): pass
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--glob", help="Only run tests matching glob")
+    parser.add_argument("-s", "--stdin", action='store_true', help="Pass the Python stdin to each test")
+    parser.add_argument("-c", "--compile", action='store_true', help="Only check compilation of target tests")
+    parser.add_argument("-r", "--run", action='store_true', help="Skip compilation. Assume the tests are already compiled, and only run the existing output")
+
+    args, remaining = parser.parse_known_args()
+    sys.argv = [sys.argv[0]] + remaining  # leave only unknown args for unittest
+
+    if not args.compile and not args.run:
+        # default behavior
+        args.run = True
+        args.compile = True
+    
+    for tc in discover_tests(args):
+        def_parts = tc.def_path.relative_to(TEST_ROOT).parts
+        code_parts = tc.code_path.relative_to(tc.def_path).parts
+        case_parts = tc.case_path.relative_to(tc.def_path).parts
+        tc.name = "test_" + "_".join(def_parts + code_parts + case_parts)
+        setattr(MyLangTestCase, tc.name, make_test_method(tc, args))
+    
     unittest.main()
 
