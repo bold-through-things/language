18e50efc6dbe47067ed518ef294affb28cec4cbc
bold-through-things <222740670+statuscallh8r@users.noreply.github.com>
2025-11-25 06:46:57 +0800

    drop a massive nuke on Python.

--- compiler/src/macros/solution_macro.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import replace
-from core.macro_registry import MacroContext, Macro_emission_provider
-
-class Solution_macro_provider(Macro_emission_provider):
-    def emission(self, ctx: MacroContext):
-        """Process all children of the solution node"""
-        for child in ctx.node.children:
-            child_ctx = replace(ctx, node=child)
-            ctx.current_step.process_node(child_ctx)
--- compiler/src/macros/utility_macros.py
+++
@@ -1,5 +0,0 @@
-from core.macro_registry import Macro_emission_provider
-
-class Noop_macro_provider(Macro_emission_provider):
-    def emission(self, _):
-        pass
